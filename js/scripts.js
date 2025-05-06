7a35a2fb82a8aa7dd481a68586459353069830ce
marlon-cesar <marlon.cesar@yahoo.com>
2025-05-06 19:25:01 -0300

    skiils section

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    setAge();
+    setExperience();
+    
+    animatedNumberInterval('projects-count', 15); 
+    animatedNumberInterval('certification-count', 1,200); 
+})
+
+
+function setAge(){
+    const now = Date.now();
+    const birthday = new Date('1998-03-02')
+
+    const ageDiff = now - birthday;
+    const ageDate = new Date(ageDiff);
+
+    const age = ageDate.getFullYear() - 1970;
+
+    animatedNumberInterval('age', age, 50);
+}
+
+function setExperience(){
+    const now = Date.now();
+    const startOfExperience = new Date('2019-05-02')
+
+    const yearDiff = now - startOfExperience;
+    const yearDate = new Date(yearDiff);
+
+    const year = yearDate.getFullYear() - 1970;
+
+    animatedNumberInterval('years-of-experience', year);    
+}
+
+
+
+function animatedNumberInterval(elementId, target, time = 100){
+    let number = 0
+    var interval = setInterval(function () {
+        document.getElementById(elementId).innerHTML = number;
+        if (number >= target) clearInterval(interval);
+        number++;
+    }, time);
+}
